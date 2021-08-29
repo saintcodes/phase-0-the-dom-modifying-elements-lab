6ddac5aeb4ca1ecfb3ad8b714a6a7c7ee4c4c066
Michelle <clairsin@gmail.com>
2021-08-28 21:51:15 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let main = document.getElementById('main');
+main.remove();
+
+
+const newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+victory.innerHTML = '<h1>Sinclair is the champion!</h1>';
